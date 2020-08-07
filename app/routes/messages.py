f97f0d96e8d8ae341ce3d22189a96bed69ef4f03
sdkag <senyo.k.agawu@gmail.com>
2020-08-07 11:53:03 -0400

    resolve conflict

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -3,54 +3,45 @@ from app.models import db, Company, Swipe, Message, Chat
 # from app.models.companies import Jobseeker
 # from ..auth import require_auth
 bp = Blueprint("messages", __name__, url_prefix='/api')
-
-@bp.route('/jobseekers/<int:jobseekerId>/chats/<int:chatId>/messages')  # fetch all jobseeker messages
+# fetch all jobseeker messages
+@bp.route('/jobseekers/<int:jobseekerId>/chats/<int:chatId>/messages')
 def grabJobseekerMessages(jobseekerId, chatId):
-    # make sure that jobseekers id matches with chat id
-    """
-    TODO:
-    """
+    # # make sure that jobseekers id matches with chat id
     currentChat = Chat.query.filter(chatId == Chat.id).one()
-    if currentChat.jobseeker_id != jobseekerId:
+    if currentChat.as_dict()['jobseekers_id'] != jobseekerId:
         return '404 ERROR'
-
-    """
-    TODO:
-    """
     messages = Message.query.filter(chatId == Message.chats_id).all()
-    data = [message.as_dict() for message in messages]  ### Need .body maybe to finish up
+    # # Need .body maybe to finish up
+    data = [(message.as_dict()['body'], message.as_dict()['role']) for message in messages]
     return {'messages': data}
-
-@bp.route('/companies/<int:companyId>/chats/<int:chatId>/messages>')  # fetch all company messages
+# # fetch all company messages
+@bp.route('/companies/<int:companyId>/chats/<int:chatId>/messages')
 def grabCompanyMessages(companyId, chatId):
-    # make sure that jobseekers id matches with chat id
-    """
-    TODO: make sure that jobseekers id matches with chat id
-    """
+    # # make sure that jobseekers id matches with chat id
     currentChat = Chat.query.filter(chatId == Chat.id).one()
-    if currentChat.company_id != companyId:
+    if currentChat.as_dict()['companies_id'] != companyId:
         return '404 ERROR'
-
-    """
-    TODO: Need .body maybe to finish up
-    """
     messages = Message.query.filter(chatId == Message.chats_id).all()
-    data = [message.as_dict() for message in messages]  ### Need .body maybe to finish up
+    # # Need .body maybe to finish up
+    data = [(message.as_dict()['body'], message.as_dict()['role']) for message in messages]
     return {'messages': data}
-
-
-@bp.route('/jobseekers/<int:jobseekerId>/chats/<int:chatId>/messages>', methods=['POST'])  # fetch a single company
-def company_id(jobseekerId, chatId):
+# post a single message as a jobseeker
+@bp.route('/jobseekers/<int:jobseekerId>/chats/<int:chatId>/messages', methods=['POST'])
+def post_jobseeker_message(jobseekerId, chatId):
     data = request.json
-    print(f"\n\n\nDATA\n{data}\n\n\n")
-    message = Message(body=data['body'], role='jobseeker',  chats_id=chatId)
+    # print(f"\n\n\nDATA\n{data}\n\n\n")
+    message = Message(body=data['body'], created_at='now', role='jobseeker', chats_id=chatId)
     db.session.add(message)
     db.session.commit()
-
-@bp.route('/companies/<int:companyId>/chats/<int:chatId>/messages>', methods=['POST'])  # fetch a single company
-def company_id(companyId, chatId):
+    # print(message.body)
+    return data['body']
+# # post a single message as a company
+@bp.route('/companies/<int:companyId>/chats/<int:chatId>/messages', methods=['POST'])
+def post_company_message(companyId, chatId):
     data = request.json
-    print(f"\n\n\nDATA\n{data}\n\n\n")
-    message = Message(body=data['body'], role='company',  chats_id=chatId)
+    # print(f"\n\n\nDATA\n{data}\n\n\n")
+    message = Message(body=data['body'], created_at='now', role="company", chats_id=chatId)
     db.session.add(message)
     db.session.commit()
+    # print(message.body)
+    return data['body']
