64e4c2ceecf021c53588666c5cccf21040872efa
sdkag <senyo.k.agawu@gmail.com>
2020-08-08 14:00:17 -0400

    update backend auth logic

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ import jwt
 from app.config import Configuration
 from app.models.users import User
 
-def require_auth(func):
+def require_auth_jobseeker(func):
   @wraps(func)
   def wrapped(*args, **kwargs):
     access_token = request.headers.get('Authorization', None)
@@ -13,8 +13,22 @@ def require_auth(func):
       return {'error': 'authentication required'}, 401
     try:
       decoded = jwt.decode(access_token, Configuration.SECRET_KEY)
-      user = User.query.filter(User.email == decoded.get('email')).first()
+      jobseeker = Jobseeker.query.filter(Jobseeker.email == decoded.get('email')).first()
     except:
       return {'error': 'invalid auth token'}, 401
-    return func(*args, authorized_user=user, **kwargs)
+    return func(*args, authorized_user=jobseeker, **kwargs)
+  return wrapped
+
+def require_auth_company(func):
+  @wraps(func)
+  def wrapped(*args, **kwargs):
+    access_token = request.headers.get('Authorization', None)
+    if not access_token:
+      return {'error': 'authentication required'}, 401
+    try:
+      decoded = jwt.decode(access_token, Configuration.SECRET_KEY)
+      company = Company.query.filter(Company.email == decoded.get('email')).first()
+    except:
+      return {'error': 'invalid auth token'}, 401
+    return func(*args, authorized_user=company, **kwargs)
   return wrapped
