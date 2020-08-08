353a2543ae2d3e8964c5fb0f35e60013f3ce3c71
Rocky <yangrocky2013@gmail.com>
2020-08-07 19:22:33 -0500

    minor fix

--- app/routes/openings.py
+++ app/routes/openings.py
@@ -11,20 +11,20 @@ def fetchall_openings():
     openings = Opening.query.all()
     company = [c.as_dict() for c in companies]
     data = [opening.as_dict() for opening in openings]
-    opening_info = []
+    openings_info = []
 
     for info in data:
         company = Company.query.filter(info['companies_id'] == Company.id).one().as_dict()
-        openingAndCompanyInfo = {}
-        openingAndCompanyInfo['image'] = company['image']
-        openingAndCompanyInfo['company_name'] = company['company_name']
-        openingAndCompanyInfo['email'] = company['email']
-        openingAndCompanyInfo['size'] = company['size']
-        openingAndCompanyInfo['location'] = company['location']
-        openingAndCompanyInfo['bio'] = company['bio']
-        openingAndCompanyInfo['title'] = info['title']
-        openingAndCompanyInfo['description'] = info['description']
-        opening_info.append(openingAndCompanyInfo)
+        openingsAndCompaniesInfo = {}
+        openingsAndCompaniesInfo['image'] = company['image']
+        openingsAndCompaniesInfo['company_name'] = company['company_name']
+        openingsAndCompaniesInfo['email'] = company['email']
+        openingsAndCompaniesInfo['size'] = company['size']
+        openingsAndCompaniesInfo['location'] = company['location']
+        openingsAndCompaniesInfo['bio'] = company['bio']
+        openingsAndCompaniesInfo['title'] = info['title']
+        openingsAndCompaniesInfo['description'] = info['description']
+        openings_info.append(openingsAndCompaniesInfo)
     # o for o in openings
 
 # c1 = session.query(Customer).options(joinedload(Customer.invoices)).filter_by(name='Govind Pant').one()
@@ -32,7 +32,7 @@ def fetchall_openings():
     
     # opening = [o.update(dict('company_name', o.company.name)) for o in openings]
     # opening = [o.as_dict() for o in openings]
-    payload = {"opening": opening_info} # how to merge?
+    payload = {"opening": openings_info} # how to merge?
     return payload
 
 @bp.route('/<int:openingId>')
@@ -43,10 +43,9 @@ def fetch_one_opening(openingId):
     return {'opening': dic}
 
 
-@bp.route('companies/<int:companyId>', methods=['POST'])  # post a new opening
+@bp.route('/companies/<int:companyId>', methods=['POST'])  # post a new opening
 def post_openings(companyId):
     data = request.json
-
     try:
         opening = Opening(
             companies_id=companyId, title=data['title'], description=data['description'], created_at='now')
