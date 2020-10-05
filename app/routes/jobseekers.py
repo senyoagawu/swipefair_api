a47a046298bf5ec21f6551eabc928ae89ad977ee
Rocky <yangrocky2013@gmail.com>
2020-10-05 11:55:29 -0500

    removed print logs

--- app/routes/jobseekers.py
+++ app/routes/jobseekers.py
@@ -85,13 +85,9 @@ def edit_jobseeker_with_image(jobseekerId):
     print(data, 'data')
 
     file = request.files['image'] 
-    print(file)
     s3_resource = boto3.resource('s3')
-    print('line 90')
     my_bucket = s3_resource.Bucket(Configuration.S3_BUCKET_NAME)
-    print("line 92")
     my_bucket.Object(file.filename).put(Body=file, ACL='public-read')
-    print("line 94")
     jobseeker = Jobseeker.query.filter(Jobseeker.id == jobseekerId).one()
     if jobseeker: 
         jobseeker.image = f'https://swipefair.s3.us-east-2.amazonaws.com/{my_bucket.Object(file.filename).key}'
